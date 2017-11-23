5d0c49084cb0219862eda4405151709c4b72512f
Gustavo Aguiar <gugahoa@gmail.com>
2017-11-23 16:13:49 -0200

    Working example of inline query answer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+extern crate telebot;
+extern crate tokio_core;
+extern crate futures;
+extern crate erased_serde;
+
+use telebot::RcBot;
+use tokio_core::reactor::Core;
+use futures::stream::Stream;
+use std::env;
+use futures::IntoFuture;
+
+use erased_serde::Serialize;
+
+use telebot::functions::*;
+use telebot::objects::*;
+
 fn main() {
-    println!("Hello, world!");
+    // Create a new tokio core
+    let mut lp = Core::new().unwrap();
+
+    // Create the bot
+    let bot = RcBot::new(lp.handle(), &env::var("TELEGRAM_BOT_KEY").unwrap()).update_interval(200);
+
+    let stream = bot.get_stream()
+        .filter_map(|(bot, msg)| {
+            println!("{:?}", msg);
+            msg.inline_query.map(|query| (bot, query))
+        })
+        .and_then(|(bot, query)| {
+            let result: Vec<Box<Serialize>> = vec![
+                Box::new(InlineQueryResultArticle::new(
+                    "Test".into(),
+                    Box::new(InputMessageContent::Text::new(
+                        "This is a test".into(),
+                    )),
+                )),
+            ];
+
+            bot.answer_inline_query(query.id, result)
+                .is_personal(true)
+                .next_offset("")
+                .switch_pm_text("")
+                .send()
+        });
+
+    // enter the main loop
+    lp.run(stream.for_each(|_| Ok(())).into_future()).unwrap();
 }
