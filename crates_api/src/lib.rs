08fb070f37c1c1f002f6c008972c238969ae73d7
Gustavo Aguiar <gugahoa@gmail.com>
2017-11-29 11:04:11 -0200

    Implement CratesError type

--- crates_api/src/lib.rs
+++ crates_api/src/lib.rs
@@ -6,6 +6,8 @@ extern crate serde_json;
 #[macro_use]
 extern crate serde_derive;
 
+use std::error;
+use std::fmt;
 
 #[derive(Deserialize, Debug)]
 pub struct Crate {
@@ -20,10 +22,54 @@ pub struct Crates {
     pub crates: Vec<Crate>,
 }
 
-pub fn query(query_string: String) -> Crates {
-    reqwest::get(
+#[derive(Debug)]
+pub enum CratesError {
+    RequestError(reqwest::Error),
+    DeserializeError(serde_json::Error),
+}
+
+impl From<reqwest::Error> for CratesError {
+    fn from(err: reqwest::Error) -> CratesError {
+        CratesError::RequestError(err)
+    }
+}
+
+impl From<serde_json::Error> for CratesError {
+    fn from(err: serde_json::Error) -> CratesError {
+        CratesError::DeserializeError(err)
+    }
+}
+
+impl error::Error for CratesError {
+    fn description(&self) -> &str {
+        match *self {
+            CratesError::RequestError(ref req_err) => req_err.description(),
+            CratesError::DeserializeError(ref serde_err) => serde_err.description(),
+        }
+    }
+
+    fn cause(&self) -> Option<&error::Error> {
+        match *self {
+            CratesError::RequestError(ref req_err) => Some(req_err),
+            CratesError::DeserializeError(ref serde_err) => Some(serde_err),
+        }
+    }
+}
+
+impl fmt::Display for CratesError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            CratesError::DeserializeError(ref serde_err) => serde_err.fmt(f),
+            CratesError::RequestError(ref req_err) => req_err.fmt(f),
+        }
+    }
+}
+
+pub fn query(query_string: String) -> Result<Crates, CratesError> {
+    let crates: Crates = reqwest::get(
         format!("https://crates.io/api/v1/crates?q={}", query_string).as_str(),
-    ).unwrap()
-        .json()
-        .unwrap()
+    )?
+        .json()?;
+
+    Ok(crates)
 }
