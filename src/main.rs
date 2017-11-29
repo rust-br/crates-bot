b408f1ceb3e0d267e19d72fb8527ed205bd05d07
Gustavo Aguiar <gugahoa@gmail.com>
2017-11-29 11:04:28 -0200

    Move inline result building to separate function

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,34 @@ use erased_serde::Serialize;
 use telebot::functions::*;
 use telebot::objects::*;
 
+fn inline_result(crates: Vec<crates_api::Crate>) -> Vec<Box<Serialize>> {
+    crates
+        .into_iter()
+        .map(|each_crate| {
+            let crate_name = each_crate.name;
+            let crate_doc = each_crate.documentation.unwrap_or("None".into());
+            let crate_desc = each_crate.description.unwrap_or("".into());
+            let crate_repo = each_crate.repository.unwrap_or("".into());
+
+            let msg_text =
+                format!(
+                "*Crate*: {}\n*Description*: {}\n*Repository*: {}\n*Doc*: {}",
+                &crate_name,
+                &crate_desc,
+                &crate_repo,
+                &crate_doc,
+                );
+            let input_message_content = InputMessageContent::Text::new(msg_text);
+            let inline_resp = InlineQueryResultArticle::new(
+                crate_name.clone().into(),
+                Box::new(input_message_content),
+            );
+
+            Box::new(inline_resp.description(crate_desc)) as Box<Serialize>
+        })
+        .collect()
+}
+
 fn main() {
     // Create a new tokio core
     let mut lp = Core::new().unwrap();
@@ -29,32 +57,19 @@ fn main() {
         })
         .and_then(|(bot, query)| {
             let crates = crates_api::query(query.query);
-            let result: Vec<Box<Serialize>> = crates
-                .crates
-                .into_iter()
-                .map(|each_crate| {
-                    let crate_name = each_crate.name;
-                    let crate_doc = each_crate.documentation.unwrap_or("None".into());
-                    let crate_desc = each_crate.description.unwrap_or("".into());
-                    let crate_repo = each_crate.repository.unwrap_or("".into());
-
-                    let msg_text =
-                        format!(
-                            "*Crate*: {}\n*Description*: {}\n*Repository*: {}\n*Doc*: {}",
-                            &crate_name,
-                            &crate_desc,
-                            &crate_repo,
-                            &crate_doc,
-                            );
-                    let input_message_content = InputMessageContent::Text::new(msg_text);
-                    let inline_resp = InlineQueryResultArticle::new(
-                        crate_name.clone().into(),
-                        Box::new(input_message_content),
-                    );
-
-                    Box::new(inline_resp.description(crate_desc)) as Box<Serialize>
-                })
-                .collect();
+            let result: Vec<Box<Serialize>> = if crates.is_ok() {
+                inline_result(crates.unwrap().crates)
+            } else {
+                println!("Error: {:?}", crates);
+                vec![
+                    Box::new(InlineQueryResultArticle::new(
+                        "Error fetching results".into(),
+                        Box::new(InputMessageContent::Text::new(
+                            "There was an error querying crates api".into(),
+                        )),
+                    )),
+                ]
+            };
 
             bot.answer_inline_query(query.id, result).send()
         });
