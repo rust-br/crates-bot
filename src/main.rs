48a3abeb8e59a8e39b67e0e01f773a41c4e0e41e
Gustavo Aguiar <gustavo.aguiar@magnetis.com.br>
2020-08-27 23:38:52 -0300

    refactor: use async/.await

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,17 @@
-use futures::future::Either;
-use futures::prelude::*;
-use futures::stream::Stream;
 use std::env;
-use telegram_bot::prelude::*;
-use telegram_bot::types::{
-    InlineKeyboardButton, InlineQueryResultArticle, InputTextMessageContent,
-};
-use telegram_bot::{Api, UpdateKind};
-use tokio_core::reactor::Core;
+use telegram_bot::types::*;
+use telegram_bot::{Api, InlineKeyboardButton};
 
-fn main() {
-    let mut core = Core::new().unwrap();
+use futures::StreamExt;
 
-    let token = env::var("TELEGRAM_BOT_KEY").expect("TELEGRAM_BOT_KEY not found in environment");
-
-    let api = Api::configure(token)
-        .build(core.handle())
-        .expect("Failed to spawn bot threads");
-    let update_stream = api.stream()
-        .map_err(|err| {
-            dbg!(&err);
-            crates_bot::Error::TelegramError(err)
-        })
-        .for_each(|update| {
-            dbg!(&update);
-            if let UpdateKind::InlineQuery(query) = update.kind {
-                let query_string = query.query.clone();
-                let query_clone = query.clone();
-                Either::A(
-                    crates_bot::search(&query_string)
-                        .map(|crates| {
-                            let crates_bot::Crates { crates } = crates;
-                            let mut ans = query_clone.answer(vec![]);
-                            for c in crates {
-                                let message_text = format!(
+async fn handle_inline_query(
+    query: InlineQuery,
+    api: &Api,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let crates_bot::Crates { crates } = crates_bot::search(&query.query).await?;
+    let mut ans = query.answer(vec![]);
+    for c in crates {
+        let message_text = format!(
                                     "<b>Crate</b>: {}\n<b>Description</b>: {}\n<b>Total downloads</b>: {}, <b>Recent downloads</b>: {}",
                                     &c.name,
                                     &c.description.clone().unwrap_or("".into()),
@@ -41,56 +19,59 @@ fn main() {
                                     &c.recent_downloads
                                 );
 
-                                let input_text_message_content = InputTextMessageContent {
-                                    message_text,
-                                    parse_mode: Some(telegram_bot::ParseMode::Html),
-                                    disable_web_page_preview: true,
-                                };
+        let input_text_message_content = InputTextMessageContent {
+            message_text,
+            parse_mode: Some(telegram_bot::ParseMode::Html),
+            disable_web_page_preview: true,
+        };
 
-                                let mut article = InlineQueryResultArticle::new(
-                                    c.name.clone(),
-                                    c.name,
-                                    input_text_message_content,
-                                );
+        let mut article =
+            InlineQueryResultArticle::new(c.name.clone(), c.name, input_text_message_content);
 
-                                if let Some(description) = c.description {
-                                    article.description(description);
-                                }
+        if let Some(description) = c.description {
+            article.description(description);
+        }
 
-                                let mut inline_keyboard_row = vec![];
-                                if let Some(repository) = c.repository {
-                                    inline_keyboard_row.push(InlineKeyboardButton::url("Repository", &repository));
-                                }
+        let mut inline_keyboard_row = vec![];
+        if let Some(repository) = c.repository {
+            inline_keyboard_row.push(InlineKeyboardButton::url("Repository", &repository));
+        }
 
-                                if let Some(crates_doc) = c.documentation {
-                                    inline_keyboard_row.push(InlineKeyboardButton::url("Documentation", &crates_doc));
-                                }
+        if let Some(crates_doc) = c.documentation {
+            inline_keyboard_row.push(InlineKeyboardButton::url("Documentation", &crates_doc));
+        }
 
-                                article.reply_markup(vec![inline_keyboard_row]);
-                                ans.add_inline_result(article);
-                            }
+        article.reply_markup(vec![inline_keyboard_row]);
+        ans.add_inline_result(article);
+    }
 
-                            api.spawn(ans);
-                        })
-                        .map_err(|err| {
-                            let mut ans = query.answer(vec![]);
-                            ans.add_inline_result(InlineQueryResultArticle::new(
-                                "random_id",
-                                "An error occurred, could not search crates.io",
-                                InputTextMessageContent {
-                                    message_text: "Error searching crates.io, could not return result".into(),
-                                    parse_mode: None,
-                                    disable_web_page_preview: false,
-                                },
-                            ));
-                            api.spawn(ans);
-                            dbg!(err.into())
-                        })
-                )
-            } else {
-                Either::B(futures::future::ok(()))
+    api.spawn(ans);
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let token = env::var("TELEGRAM_BOT_KEY").expect("TELEGRAM_BOT_KEY not found in environment");
+
+    let api = Api::new(token);
+
+    let mut stream = api.stream();
+    while let Some(update) = stream.next().await {
+        match update {
+            Ok(Update {
+                kind: UpdateKind::InlineQuery(query),
+                id: _,
+            }) => {
+                let _ignore = dbg!("handle result = {}", handle_inline_query(query, &api).await);
             }
-        });
+            Ok(update_kind) => {
+                dbg!("received non supported update_kind = {:?}", update_kind);
+            }
+            Err(err) => {
+                dbg!("update error = {}", err);
+            }
+        }
+    }
 
-    core.run(update_stream).expect("Failed to run react");
+    Ok(())
 }
